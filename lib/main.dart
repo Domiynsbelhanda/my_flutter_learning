cb4f62bf414128c8d35144d6f98359985dc8f7ef
Dominique Youness <youness.dominique@gmail.com>
2022-08-26 15:31:33 +0200

    [FEAT] Create HomeController

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 import 'BLoC/counter.dart';
+import 'Get/CounterApp/home.dart';
 
 void main() {
-  runApp(const Counter());
+  // runApp(const Counter());
+  runApp(
+      GetMaterialApp(home: Home())
+  );
 }
